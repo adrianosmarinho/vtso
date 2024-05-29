f0789b86201b3228e0c19c77dd0208fc7c3b5a1b
Adriano Marinho <adriano.marinho@credfin.io>
2024-05-29 21:32:17 +1000

    refactor: replace repeated code with a api_client fixture

--- vtso/tests/views/test_company.py
+++ vtso/tests/views/test_company.py
@@ -14,6 +14,14 @@ class TestCompanyList:
     Unit tests for vtso/companies
     """
 
+    @pytest.fixture
+    def api_client_authenticated(self):
+        user = User.objects.create(username="test_user")
+        token = Token.objects.create(user=user)
+        client = APIClient()
+        client.force_authenticate(user=user, token=token)
+        return client
+
     def test_list_companies_get_not_authenticated(self):
         """
         GET /companies should return 401 because this request is not authenticated.
@@ -32,56 +40,42 @@ class TestCompanyList:
             response.data["detail"] == "Authentication credentials were not provided."
         )
 
-    def test_list_companies_empty_name_get(self):
+    def test_list_companies_empty_name_get(self, api_client_authenticated):
         """
         GET /companies should return 200 because our Model
         and View allow for the creation of a Company with empty name
         """
         # Arrange
-        user = User.objects.create(username="test_user")
-        token = Token.objects.create(user=user)
-        client = APIClient()
-        client.force_authenticate(user=user, token=token)
         CompanyFactory(name="")
         url = reverse("companies")
 
         # Act
-        response = client.get(url)
+        response = api_client_authenticated.get(url)
 
         # Assert
         assert response.status_code == status.HTTP_200_OK
 
-    def test_list_companies_happy_path_get(self):
+    def test_list_companies_happy_path_get(self, api_client_authenticated):
         # Arrange
-        client = APIClient()
-        user = User.objects.create(username="test_user")
-        token = Token.objects.create(user=user)
-        client = APIClient()
-        client.force_authenticate(user=user, token=token)
         CompanyFactory(name="Company A")
         CompanyFactory(name="Company B")
         expected_response_length = 2
         url = reverse("companies")
 
         # Act
-        response = client.get(url)
+        response = api_client_authenticated.get(url)
 
         # Assert
         assert response.status_code == status.HTTP_200_OK
         assert len(response.data) == expected_response_length
 
-    def test_list_companies_happy_path_post(self):
+    def test_list_companies_happy_path_post(self, api_client_authenticated):
         # Arrange
-        client = APIClient()
-        user = User.objects.create(username="test_user")
-        token = Token.objects.create(user=user)
-        client = APIClient()
-        client.force_authenticate(user=user, token=token)
         name = "Company C"
         url = reverse("companies")
 
         # Act
-        response = client.post(url, data={"name": name})
+        response = api_client_authenticated.post(url, data={"name": name})
 
         # Assert
         assert response.status_code == status.HTTP_201_CREATED
