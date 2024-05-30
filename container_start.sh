7482f8e71c9559490d75a33075ff4348741d236c
Adriano Marinho <adriano.marinho@credfin.io>
2024-05-30 21:13:27 +1000

    feat: add command to generate schema on startup

--- container_start.sh
+++ container_start.sh
@@ -1,3 +1,5 @@
 # We should migrate on startup in case there's been any db changes
 python3 manage.py migrate
+# Ensure our OpenAPI schema is up to date
+python manage.py spectacular --file schema.yaml
 python manage.py runserver 0.0.0.0:8000
